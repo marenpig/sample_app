db34430b7c7140fcc6194e7c80be910da7b961e6
Maren <maren.brown@gmail.com>
2013-03-31 15:29:24 -0400

    Chapter 5 update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
   
   # Returns the full title on a per-page basis.
   def full_title(page_title)
@@ -10,6 +9,3 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
-=======
->>>>>>> 18e67401e5e34b2283ca923c4112e7e1e5be0c01
-end

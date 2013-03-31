e1282776fe2ba888f8852c1cab62cdb153617c72
Maren <maren.brown@gmail.com>
2013-03-31 15:16:16 -0400

    chapter 5 update

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,15 @@
+module ApplicationHelper
+<<<<<<< HEAD
+  
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+=======
+>>>>>>> 18e67401e5e34b2283ca923c4112e7e1e5be0c01
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '057193249aafe1b26974609c45b150774f0ed8106c3547b77e432c9dd27cff18b6fe29f94a47be8b33413632d28f6da0af3d922340ef950635a3629c86f01c84'
